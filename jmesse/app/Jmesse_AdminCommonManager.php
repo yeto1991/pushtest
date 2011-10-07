1fb9ab79311e9fdfb78b59b79bfd6107bd7a03ca
m.sasaki <m.sasaki@9f050dc0-3a10-437a-a76f-4ba47af507b8>
2011-10-07 08:32:12 +0000

    新規登録。
    git-svn-id: http://192.168.0.212:18080/svn/j-messe@10 9f050dc0-3a10-437a-a76f-4ba47af507b8

---
+++ jmesse/app/Jmesse_AdminCommonManager.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ *  Jmesse_AdminCommonManager.php
+ *
+ *  @author     {$author}
+ *  @package    Jmesse
+ *  @version    $Id: d4af361a99e2aaa95cedee2132d1ca3f10920c6b $
+ */
+
+/**
+ *  Jmesse_AdminCommonManager
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Jmesse
+ */
+class Jmesse_AdminCommonManager extends Ethna_AppManager
+{
+	/**
+	 * ユーザ管理ページを表示可能であるかチェック。
+	 *
+	 * @return true：可、false：否。
+	 */
+	function isLoginUser() {
+
+		$ret = true;
+
+		if (!$this->session->isStart()) {
+			// Sessionが開始していない。
+			$ret = false;
+		} else if (null == $this->session->get('username') || '' == $this->session->get('username')) {
+			// ログインユーザが設定されていない。
+			$ret = false;
+		} else if ('1' != $this->session->get('auth_user')) {
+			// 権限なし
+			$ret = false;
+		}
+
+		return $ret;
+	}
+
+	/**
+	 * 展示会管理ページを表示可能であるかチェック。
+	 *
+	 * @return true：可、false：否。
+	 */
+	function isLoginFair() {
+
+		$ret = true;
+
+		if (!$this->session->isStart()) {
+			// Sessionが開始していない。
+			$ret = false;
+		} else if (null == $this->session->get('username') || '' == $this->session->get('username')) {
+			// ログインユーザが設定されていない。
+			$ret = false;
+		} else if ('1' != $this->session->get('auth_fair')) {
+			// 権限なし
+			$ret = false;
+		}
+
+		return $ret;
+	}
+
+	/**
+	 * ログテーブルに記録する。
+	 *
+	 * @param string $user_id ログインユーザID。
+	 * @param string $ope_kbn '1'：参照、'2'：登録、'3'：更新、'4'：削除、'5'：その他。
+	 * @param string $info_kbn '1'：ユーザ情報、'2'：見本市情報、'3'：その他。
+	 * @param string $remarks ユーザ情報の場合はユーザID、見本市情報の場合は見本市番号を設定。
+	 *
+	 * @return 0：正常、Ethna_Error：エラー。
+	 */
+	function regLog($user_id, $ope_kbn, $info_kbn, $remarks) {
+		// オブジェクトを取得
+		$jm_log = $this->backend->getObject('JmLog');
+		if (null == $jm_log) {
+			$this->backend->getLogger()->log(LOG_ERR, 'JmLogオブジェクト取得失敗');
+			return Ethna::raiseError('JmLogオブジェクト取得に失敗しました。', E_FAIL_TO_GET_OBJECT_JM_LOG);
+		}
+
+		// 項目設定
+		$jm_log->set('user_id', $user_id);
+		$jm_log->set('ope_kbn', $ope_kbn);
+		$jm_log->set('info_kbn', $info_kbn);
+		$jm_log->set('remarks', $remarks);
+		$jm_log->set('reg_date', date('Y/m/d H:i:s'));
+
+		// INSERT
+		$ret = $jm_log->add();
+
+		if (Ethna::isError($ret)) {
+			$this->backend->getLogger()->log(LOG_ERR, 'JM_LOGの登録に失敗しました。');
+			return $ret;
+		}
+
+		return 0;
+	}
+}
+?>
