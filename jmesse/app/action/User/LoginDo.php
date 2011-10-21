8f00d89b7827643cc9d9c482475556bb156403ee
m.sasaki <m.sasaki@9f050dc0-3a10-437a-a76f-4ba47af507b8>
2011-10-21 05:08:06 +0000

    フロント ユーザ管理 ログイン、メニュー　形だけ作成
    git-svn-id: http://192.168.0.212:18080/svn/j-messe@82 9f050dc0-3a10-437a-a76f-4ba47af507b8

---
+++ jmesse/app/action/User/LoginDo.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ *  User/LoginDo.php
+ *
+ *  @author     {$author}
+ *  @package    Jmesse
+ *  @version    $Id: 6dbb28cac61a23f06dba884c38c304aed3dcc84b $
+ */
+
+require_once 'Login.php';
+require_once 'Jmesse_JmUser.php';
+
+/**
+ *  user_loginDo Form implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Jmesse
+ */
+class Jmesse_Form_UserLoginDo extends Jmesse_Form_UserLogin
+{
+}
+
+/**
+ *  user_loginDo action implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Jmesse
+ */
+class Jmesse_Action_UserLoginDo extends Jmesse_ActionClass
+{
+	/**
+	 *  preprocess of user_loginDo Action.
+	 *
+	 *  @access public
+	 *  @return string    forward name(null: success.
+	 *                                false: in case you want to exit.)
+	 */
+	function prepare()
+	{
+		if ($this->af->validate() > 0) {
+			return 'user_login';
+		}
+
+		return null;
+	}
+
+	/**
+	 *  user_loginDo action implementation.
+	 *
+	 *  @access public
+	 *  @return string  forward name.
+	 */
+	function perform()
+	{
+		$login_ok = true;
+
+		// ユーザ情報取得
+		$user =& $this->backend->getObject('JmUser', 'email', $this->af->get('email'));
+
+		// ユーザ認証
+		if (null == $user || null == $user->get('user_id')) {
+			$this->backend->getLogger()->log(LOG_DEBUG, 'ユーザが未登録');
+			$login_ok = false;
+		} else if ($this->af->get('password') != $user->get('password')) {
+			$this->backend->getLogger()->log(LOG_DEBUG, 'パスワード相違');
+			$login_ok = false;
+		} else if ('1' != $user->get('auth_gen')) {
+			$this->backend->getLogger()->log(LOG_DEBUG, '管理権限なし');
+			$login_ok = false;
+		}
+
+		$mgr = $this->backend->getManager('userCommon');
+		if ($login_ok) {
+			// ログに記録
+			$ret = $mgr->regLog($user->get('user_id'), '5', '3', 'Successful login.');
+			if (Ethna::isError($ret)) {
+				$this->ae->addObject('error', $ret);
+				return 'error';
+			}
+
+			// SESSIONに設定
+			$this->session->start();
+			$this->session->set('user_id', $user->get('user_id'));
+			$this->session->set('auth_gen', $user->get('auth_gen'));
+			$ret_view = 'user_top';
+		} else {
+			// ログイン失敗画面へ遷移
+			$ret = $mgr->regLog('0', '5', '3', 'Login failed.('.$this->af->get('email').':'.$_SERVER['REMOTE_ADDR'].')');
+			if (Ethna::isError($ret)) {
+				$this->ae->addObject('error', $ret);
+				return 'error';
+			}
+			$this->ae->add('error', 'Eメールまたはパスワードが間違っています。');
+			return 'user_login';
+		}
+
+		// 転送
+		if (null != $this->af->get('function') && '' != $this->af->get('function')) {
+			header('Location: '.$this->af->get('function'));
+		}
+
+		// TOP画面へ遷移
+		return $ret_view;
+	}
+}
+
+?>
---
+++ jmesse/template/ja_JP/user/top.tpl
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+	<div align="center">
+		<table>
+			<tr>
+				<td>主催者管理メニュー<br/><br/><br/></td>
+			</tr>
+			<tr>
+				<td align="left">
+					<b>ユーザ情報メニュー</b><br />
+					<a href="?action_user_userDetail=true">ユーザ登録内容確認</a><br/>
+					<br/><br/><br/>
+					<b>展示会情報メニュー</b><br/>
+					<a href="{$config.url}?action_user_fairRegist=true">新規展示会情報登録</a><br/>
+					<a href="{$config.url}?action_user_fairSearch=true">登録済みの展示会情報確認</a><br/>
+				</td>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
