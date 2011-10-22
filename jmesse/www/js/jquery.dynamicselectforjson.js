9a054146ddd6c5b6e52d22d84dc5fcc54a39cde7
m.sasaki <m.sasaki@9f050dc0-3a10-437a-a76f-4ba47af507b8>
2011-10-22 01:22:01 +0000

    git-svn-id: http://192.168.0.212:18080/svn/j-messe@88 9f050dc0-3a10-437a-a76f-4ba47af507b8

--- jmesse/www/js/jquery.dynamicselectforjson.js
+++ jmesse/www/js/jquery.dynamicselectforjson.js
@@ -2,16 +2,18 @@ function dynamicpulldownlist(url, postData, targetId, selected) {
 	$.getJSON(url, postData, function(json) {
 		var jsonDataObj = eval(json);
 		var dataArray = [];
-		for ( var i = 0; i < jsonDataObj.length; i++) {
-			var tmp = {};
-			tmp["value"] = jsonDataObj[i].value;
-			tmp["text"] = jsonDataObj[i].text;
-			dataArray.push(tmp);
-		}
-		var select = $(targetId);
-		select.clearOptions();
-		for ( var i = 0; i < dataArray.length; i++) {
-			select.addOption(dataArray[i]["text"], dataArray[i]["value"], dataArray[i]["value"] == selected);
+		if (null != jsonDataObj) {
+			for ( var i = 0; i < jsonDataObj.length; i++) {
+				var tmp = {};
+				tmp["value"] = jsonDataObj[i].value;
+				tmp["text"] = jsonDataObj[i].text;
+				dataArray.push(tmp);
+			}
+			var select = $(targetId);
+			select.clearOptions();
+			for ( var i = 0; i < dataArray.length; i++) {
+				select.addOption(dataArray[i]["text"], dataArray[i]["value"], dataArray[i]["value"] == selected);
+			}
 		}
 	});
 }
