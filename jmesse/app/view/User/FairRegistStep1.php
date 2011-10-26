0a545b1e5309f50471a0018038eb7cef8096ed94
m.sasaki <m.sasaki@9f050dc0-3a10-437a-a76f-4ba47af507b8>
2011-10-26 02:05:20 +0000

    ひとまず作成
    git-svn-id: http://192.168.0.212:18080/svn/j-messe@105 9f050dc0-3a10-437a-a76f-4ba47af507b8

---
+++ jmesse/app/view/User/FairRegistStep1.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *  User/FairRegistStep1.php
+ *
+ *  @author     {$author}
+ *  @package    Jmesse
+ *  @version    $Id: 47c269115c9380915eda42c4fa1c780886c064d8 $
+ */
+
+/**
+ *  user_fairRegistStep1 view implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Jmesse
+ */
+class Jmesse_View_UserFairRegistStep1 extends Jmesse_ViewClass
+{
+    /**
+     *  preprocess before forwarding.
+     *
+     *  @access public
+     */
+    function preforward()
+    {
+    }
+}
+
+?>
