fa5e9dde44fb086be665980d9708e40f10502eb4
m.sasaki <m.sasaki@9f050dc0-3a10-437a-a76f-4ba47af507b8>
2011-11-16 04:49:44 +0000

    英語化
    git-svn-id: http://192.168.0.212:18080/svn/j-messe@233 9f050dc0-3a10-437a-a76f-4ba47af507b8

--- jmesse/app/action/User/EnFairRegistStep1.php
+++ jmesse/app/action/User/EnFairRegistStep1.php
@@ -1376,7 +1376,7 @@ class Jmesse_Action_UserEnFairRegistStep1 extends Jmesse_ActionClass
 		// 見本市番号
 		if ('c' == $this->af->get('mode') || 'e' == $this->af->get('mode')) {
 			if ('' == $this->af->get('mihon_no')) {
-				$this->ae->add('error', '見本市番号がありません');
+				$this->ae->add('error', 'mihon_no error');
 			}
 		}
 
@@ -1412,17 +1412,17 @@ class Jmesse_Action_UserEnFairRegistStep1 extends Jmesse_ActionClass
 				$jm_fair_obj = $this->backend->getObject('JmFair', 'mihon_no', $this->af->get('mihon_no'));
 				if (null == $jm_fair_obj) {
 					$this->backend->getLogger()->log(LOG_ERR, '■見本市情報が存在しません。');
-					$this->ae->add('error', '見本市情報が存在しません');
+					$this->ae->add('error', 'Fair Information does not exist.');
 					return 'error';
 				}
 				if ('1' == $jm_fair_obj->get('del_flg')) {
 					$this->backend->getLogger()->log(LOG_ERR, '■見本市情報は削除されました。削除時刻('.$this->session->get('del_date').')');
-					$this->ae->add('error', '見本市情報は'.$this->session->get('del_date').'に削除されました');
+					$this->ae->add('error', 'The Fair was deleted by '.$this->session->get('del_date').'.');
 					return 'error';
 				}
 				if ($this->session->get('user_id') != $jm_fair_obj->get('user_id')) {
 					$this->backend->getLogger()->log(LOG_ERR, '■他人の見本市情報です。('.$this->session->get('user_id').', '.$jm_fair_obj->get('user_id').')');
-					$this->ae->add('error', '他人の見本市情報なので編集できません');
+					$this->ae->add('error', 'The Fair can not edit because it is not yours.');
 					return 'error';
 				}
 
@@ -1442,7 +1442,7 @@ class Jmesse_Action_UserEnFairRegistStep1 extends Jmesse_ActionClass
 			$user_obj = $this->backend->getObject('JmUser', 'user_id', $this->session->get('user_id'));
 			if (null == $user_obj) {
 				$this->backend->getLogger()->log(LOG_DEBUG, '■ユーザ情報が存在しません。');
-				$this->ae->add('error', 'ユーザ情報が存在しません');
+				$this->ae->add('error', 'User error');
 				return 'error';
 			}
 			$this->session->set('email', $user_obj->get('email'));
