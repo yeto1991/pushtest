5197e0fbebf430bd253c99a87b780739132877d5
m.sasaki <m.sasaki@9f050dc0-3a10-437a-a76f-4ba47af507b8>
2011-11-18 06:22:05 +0000

    JSONAPI 新規作成
    git-svn-id: http://192.168.0.212:18080/svn/j-messe@285 9f050dc0-3a10-437a-a76f-4ba47af507b8

---
+++ jmesse/app/action_cli/Json/GetJsonMonthlyRanking.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ *  Json/GetJsonMonthlyRanking.php
+ *
+ *  @author     {$author}
+ *  @package    Jmesse
+ *  @version    $Id: a99a32157780abedaf1b817cf022da94c2d1572c $
+ */
+
+/**
+ *  json_getJsonMonthlyRanking Form implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Jmesse
+ */
+class Jmesse_Cli_Form_JsonGetJsonMonthlyRanking extends Jmesse_ActionForm
+{
+}
+
+/**
+ *  json_getJsonMonthlyRanking action implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Jmesse
+ */
+class Jmesse_Cli_Action_JsonGetJsonMonthlyRanking extends Jmesse_ActionClass
+{
+	/**
+	 *  preprocess of json_getJsonMonthlyRanking Action.
+	 *
+	 *  @access public
+	 *  @return string    forward name(null: success.
+	 *                                false: in case you want to exit.)
+	 */
+	function prepare()
+	{
+		return null;
+	}
+
+	/**
+	 *  json_getJsonMonthlyRanking action implementation.
+	 *
+	 *  @access public
+	 *  @return string  forward name.
+	 */
+	function perform()
+	{
+		//TODO
+		return null;
+	}
+}
+
+?>
---
+++ jmesse/app/action_cli/Json/GetJsonNewMihonichi.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ *  Json/GetJsonNewMihonichi.php
+ *
+ *  @author     {$author}
+ *  @package    Jmesse
+ *  @version    $Id: a99a32157780abedaf1b817cf022da94c2d1572c $
+ */
+
+/**
+ *  json_getJsonNewMihonichi Form implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Jmesse
+ */
+class Jmesse_Cli_Form_JsonGetJsonNewMihonichi extends Jmesse_ActionForm
+{
+}
+
+/**
+ *  json_getJsonNewMihonichi action implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    Jmesse
+ */
+class Jmesse_Cli_Action_JsonGetJsonNewMihonichi extends Jmesse_ActionClass
+{
+	/**
+	 *  preprocess of json_getJsonNewMihonichi Action.
+	 *
+	 *  @access public
+	 *  @return string    forward name(null: success.
+	 *                                false: in case you want to exit.)
+	 */
+	function prepare()
+	{
+		return null;
+	}
+
+	/**
+	 *  json_getJsonNewMihonichi action implementation.
+	 *
+	 *  @access public
+	 *  @return string  forward name.
+	 */
+	function perform()
+	{
+		//TODO
+		return null;
+	}
+}
+
+?>
