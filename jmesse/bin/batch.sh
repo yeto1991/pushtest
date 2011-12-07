229359716eec14521c4f26e9885dde616340bfd3
m.sasaki <m.sasaki@9f050dc0-3a10-437a-a76f-4ba47af507b8>
2011-12-07 00:18:58 +0000

    git-svn-id: http://192.168.0.212:18080/svn/j-messe@403 9f050dc0-3a10-437a-a76f-4ba47af507b8

--- jmesse/bin/batch.sh
+++ jmesse/bin/batch.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
-
 PHP_HOME=/usr/local/php
-MYSQL_HOME=/opt/mysql
-JMESSE_HOME=/home/j-messe/app/jmesse
-DB_SERVER=localhost
+MYSQL_HOME=/home/jmesse1112/opt/mysql
+JMESSE_HOME=/home/jmesse1112/app/jmesse
+DB_SERVER=10.11.100.103
 
 # Batch開始時刻
 echo -n Batch Start :
 date
-
 $MYSQL_HOME/bin/mysql -ujmesse -pidsjmesse -h$DB_SERVER jmesse < $JMESSE_HOME/bin/summaryCountForFairCntTable.sql
+echo -n Step.01 Finish :
+date
 $PHP_HOME/bin/php -f $JMESSE_HOME/bin/getJsonRegion.php
+echo -n Step.02 Finish :
+date
 $PHP_HOME/bin/php -f $JMESSE_HOME/bin/getJsonIndustory.php
+echo -n Step.03 Finish :
+date
 $PHP_HOME/bin/php -f $JMESSE_HOME/bin/getJsonMonthlyRanking.php
+echo -n Step.04 Finish :
+date
 $PHP_HOME/bin/php -f $JMESSE_HOME/bin/getJsonNewMihonichi.php
+echo -n Step.05 Finish :
+date
 $PHP_HOME/bin/php -f $JMESSE_HOME/bin/getErrMail.php
-
 # Batch終了時刻
 echo -n Batch Finish :
 date
