3b3f3cc341570b9eb1a54b0f6358934f909a6bcf
m.sasaki <m.sasaki@9f050dc0-3a10-437a-a76f-4ba47af507b8>
2011-12-12 06:07:27 +0000

    不要メソッド削除。
    git-svn-id: http://192.168.0.212:18080/svn/j-messe@429 9f050dc0-3a10-437a-a76f-4ba47af507b8

--- jmesse/app/Jmesse_CommonManager.php
+++ jmesse/app/Jmesse_CommonManager.php
@@ -23,16 +23,6 @@ class Jmesse_CommonManager extends Ethna_AppManager
 	 *
 	 */
 	function setExtHtml() {
-// 		$opts = array(
-// 			'http' => array(
-// 				'method' => "GET",
-// 				'header' => "Content-Type: text/html; charset=utf-8",
-// 				'timeout' => 10
-// 			)
-// 		);
-// 		$context = stream_context_create($opts);
-// 		$this->af->setAppNe('header', $this->_curl_get_contents($this->config->get('header_url')));
-// 		$this->af->setAppNe('header', $this->_file_get_contents('localhost/jmesse/www/header.html'));
 		$this->af->setAppNe('header', @file_get_contents($this->config->get('header_url')));
 		$this->af->setAppNe('footer', @file_get_contents($this->config->get('footer_url')).@file_get_contents($this->config->get('footer_script_rul')));
 		$this->af->setAppNe('left_menu', @file_get_contents($this->config->get('left_menu_url')));
@@ -48,38 +38,5 @@ class Jmesse_CommonManager extends Ethna_AppManager
 		$this->af->setAppNe('left_menu', @file_get_contents($this->config->get('left_menu_url_en')));
 	}
 
-
-	function _file_get_contents($server = '', $timeout = 10){
-		$data = '';
-		if(isset($server)){
-			$fp = fsockopen($server, 80, $errno, $errstr, $timeout);
-			if($fp){
-				while($eof_check = fread($fp, 1024)){
-					$data .= $eof_check;
-				}
-				fclose($fp);
-			}else{
-				$this->backend->getLogger()->log(LOG_DEBUG, '■return false ①');
-				return false;
-			}
-		}else{
-			$this->backend->getLogger()->log(LOG_DEBUG, '■return false ②');
-			return false;
-		}
-		$this->backend->getLogger()->log(LOG_DEBUG, '■OK');
-		return $data;
-	}
-
-
-	function _curl_get_contents( $url, $timeout = 60 ){
-		$ch = curl_init();
-		curl_setopt( $ch, CURLOPT_URL, $url );
-		curl_setopt( $ch, CURLOPT_HEADER, false );
-		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-		curl_setopt( $ch, CURLOPT_TIMEOUT, $timeout );
-		$result = curl_exec( $ch );
-		curl_close( $ch );
-		return $result;
-	}
 }
 ?>
