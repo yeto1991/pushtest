818fad697ece7d9fbbe648224658b2d21ba953ad
m.sasaki <m.sasaki@9f050dc0-3a10-437a-a76f-4ba47af507b8>
2012-02-01 06:44:59 +0000

    2012/02/01 保守対応 ロジック修正
    git-svn-id: http://192.168.0.212:18080/svn/j-messe@576 9f050dc0-3a10-437a-a76f-4ba47af507b8

--- jmesse/app/action/User/UserChangeDo.php
+++ jmesse/app/action/User/UserChangeDo.php
@@ -55,7 +55,7 @@ class Jmesse_Action_UserUserChangeDo extends Jmesse_ActionClass
 			if (null == $jm_user || $this->af->get('user_id') != $jm_user->get('user_id')) {
 				$this->backend->getLogger()->log(LOG_ERR, 'ユーザ検索エラー');
 				$this->ae->add('error', 'システムエラーが発生しました。');
-				return 'enError';
+				return 'error';
 			}
 			//Form値設定
 			$this->af->set('email', $jm_user->get('email'));
